66cf3328b9d0bbf8d7d8d763055718a0c32057cf
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-03-15 14:29:52 +0100

    created client

---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <p class="error" v-if="error">{{error}}</p>
+  <div class="posts-container">
+    <div class="posts" v-for="( post,index) in posts" 
+      v-bind:item="post"
+      v-bind:index="index"
+      v-bind:key="post._id"
+    >
+      <p class="text">{{post.text}}</p>
+    </div>
+    </div>
+</div>
+ 
+</template>
+
+<script>
+import PostService from "../PostService"
+export default {
+  name: 'PostComponent',
+  data(){
+    return{
+      posts: [],
+      error: '',
+      text: ''
+    }
+  },
+
+  async created(){
+    try{
+      this.post = await PostService.getPosts()
+    }catch(err){
+      this.error = err.message
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+hbgvfc
+
